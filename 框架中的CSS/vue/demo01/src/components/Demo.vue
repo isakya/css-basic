7d4c95dd9e98bd43202210f192d49afe20cd9fc4
konglong <1016361268@qq.com>
2023-01-16 23:34:37 +0800

    vue的两种隔离样式方案

---
+++ 框架中的CSS/vue/demo01/src/components/Demo.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- vue隔离样式方案：1、模块； 2、属性 -->
+
+  <!-- @1 -->
+  <!-- <div :class="$style.demo">Hello, I'm {{ name }}</div> -->
+
+  <!-- @2 -->
+  <div class="demo">Hello, I'm {{ name }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Demo',
+  data() {
+    return {
+      name: 'demo'
+    }
+  }
+}
+</script>
+<!-- 模块 -->
+<!-- @1 -->
+<!-- <style module> -->
+
+<!-- 属性 -->
+<!-- @1 -->
+
+<style scoped>
+.demo {
+  color: red;
+}
+</style>
